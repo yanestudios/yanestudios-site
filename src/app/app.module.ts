00a87aab64717bfb8cfee56c1e9f18bc89fded63
matheuscruzsouza <matheuscruzsouza@gmail.com>
2020-06-17 22:41:32 -0300

    Layout 1.0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ProjectsComponent } from './pages/projects/projects.component';
 import { HomeComponent } from './core/home/home.component';
+import { MenuComponent } from './shared/menu/menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProjectsComponent,
-    HomeComponent
+    HomeComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
